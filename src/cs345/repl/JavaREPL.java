791ec5ff07c2942d0d5c83d705fe409f304c9188
parrt <parrt@cs.usfca.edu>
2016-01-26 10:35:35 -0800

    initial add (dup from cs652)

---
+++ src/cs345/repl/JavaREPL.java
@@ -0,0 +1,20 @@
+package cs345.repl;
+
+import java.io.IOException;
+
+public class JavaREPL {
+	public static void main(String[] args) throws IOException {
+		exec(new InputStreamReader(System.in));
+	}
+
+	public static void exec(Reader r) throws IOException {
+		BufferedReader stdin = new BufferedReader(r);
+		NestedReader reader = new NestedReader(stdin);
+		int classNumber = 0;
+		while (true) {
+			System.out.print("> ");
+			String java = reader.getNestedString();
+			// TODO
+		}
+	}
+}
